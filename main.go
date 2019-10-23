c0864d700fc731fd02907bf5aaa1761ea3a451b1
Cynthia Revstrom <me@cynthia.re>
2019-10-23 10:41:33 +0200

    test

---
+++ main.go
@@ -0,0 +1,9 @@
+//go:generate git clone https://github.com/bitcynth/please.git && cd please && make && sudo make install && echo uwu
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("UwU!")
+}
